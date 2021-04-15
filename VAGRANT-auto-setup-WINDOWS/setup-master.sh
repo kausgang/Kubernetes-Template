5ef266089068e4a33c22f4fda5ed3729eb8cd519
kausgang <write.to.kaustav@gmail.com>
2021-04-14 23:43:18 -0400

    vagrant nfs + kube cluster

--- VAGRANT-auto-setup-WINDOWS/setup-master.sh
+++ VAGRANT-auto-setup-WINDOWS/setup-master.sh
@@ -1,43 +1,54 @@
 #!/usr/bin/bash
 
-apt-get update
-apt-get install -y docker.io
-systemctl enable docker
-systemctl start docker
-apt-get install -y curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-# apt-get install -y kubeadm kubelet kubectl
-apt-get install -y kubeadm=1.21.0-00 kubelet=1.21.0-00 kubectl=1.21.0-00
-apt-mark hold kubeadm kubelet kubectl
-swapoff -a
 
-kubeadm init --pod-network-cidr=10.244.0.0/16 2>&1 | tee join.txt
-# kubeadm init --pod-network-cidr=192.168.0.0/16 2>&1 | tee join.txt
+
+# kubeadm init --pod-network-cidr=10.244.0.0/16 2>&1 | tee join.txt  # ------- for flannel
+# kubeadm init --pod-network-cidr=192.168.0.0/16 2>&1 | tee join.txt # --------- for calico
+
+echo "[TASK] Pull required containers"
+kubeadm config images pull >/dev/null 2>&1
+
+echo "[TASK] Initialize Kubernetes Cluster"
+kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
+
+echo "[TASK] Deploy Calico network"
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml >/dev/null 2>&1
+
+
+echo "[TASK] Setup kubeconfig"
 mkdir -p /home/vagrant/.kube
 cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-chown $(id -u):$(id -g) /home/vagrant/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube/config
+
+cp -i /etc/kubernetes/admin.conf /vagrant_data/config
 
 #using flannel for virtual network
 #see https://kubernetes.io/docs/concepts/cluster-administration/addons/ for other options
 
 # curl https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml -O
 # kubectl apply -f kube-flannel.yml
-kubectl apply -f /vagrant_data/kube-flannel.yml
 
 
-# echo "[TASK 3] Deploy Calico network"
+# Deploy Calico network"
 # kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml 
 
-
-printf "\n\n"
-
-# tail -20 join.txt
+echo "[TASK] Generate and save cluster join command"
+kubeadm token create --print-join-command > /vagrant_data/JOIN_NETWORK.sh
 
 
-tail -2 join.txt > /vagrant_data/JOIN_NETWORK.sh
-cp /home/vagrant/.kube/config /vagrant_data/config
-chmod 755 /home/vagrant/.kube/config
 
-rm join.txt
-# rm kube-flannel.yml
+echo "[TASK] Downloading and Installing the mount Components"
+apt update -qq >/dev/null 2>&1
+sudo apt install -qq -y nfs-kernel-server 
+
+
+echo "[TASK] Exporting a General Purpose Mount"
+mkdir /var/nfs/PV -p
+chown nobody:nogroup /var/nfs/PV
+
+echo "[TASK] Configuring the NFS Exports on the Host Server"
+cat >>/etc/exports<<EOF
+/var/nfs/PV worker-1(rw,sync,no_subtree_check)
+EOF
+systemctl restart nfs-kernel-server
+
--- VAGRANT-auto-setup-WINDOWS/setup-worker.sh
+++ VAGRANT-auto-setup-WINDOWS/setup-worker.sh
@@ -1,62 +1,22 @@
 #!/usr/bin/bash
 
-#using tutorial https://phoenixnap.com/kb/install-kubernetes-on-ubuntu
-#make sure the network deployment is done the way this script is written, and not the website way
-
-printf "Enter the password for `whoami` :- "
-read password
-printf "Enter what hostname do you want for the WORKER node :- "
-read node_name
-printf "Enter MASTER node's IP address :- "
-read master_IP
-printf "Enter username for the MASTER_NODE :- "
-read master_user
-printf "Enter password for user in MASTER_NODE :- "
-read master_password
 
 
+echo "[TASK] Join cluster"
+bash /vagrant_data/JOIN_NETWORK.sh
 
-if [ `echo $password` == '' ]
-then
-echo "Enter valid password for the user installing kubernetes"
-exit 1
-fi
-
-
-if [ `echo $node_name` == '' ]
-then
-echo "Enter valid hostname for the control plane"
-exit 1
-fi
-#!/usr/bin/bash
-
-
-apt-get update
-apt-get install -y docker.io
-systemctl enable docker
-systemctl start docker
-apt-get install -y curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-# apt-get install -y kubeadm kubelet kubectl
-apt-get install -y kubeadm=1.21.0-00 kubelet=1.21.0-00 kubectl=1.21.0-00
-apt-mark hold kubeadm kubelet kubectl
-swapoff -a
-
-
-
-printf "\n\n"
-
-# chmod 755 /home/vagrant/.kube/config
-/vagrant_data/JOIN_NETWORK.sh
-
+echo "[TASK] Setup kubeconfig"
 mkdir -p /home/vagrant/.kube
+chown vagrant:vagrant /home/vagrant/.kube
 cp /vagrant_data/config /home/vagrant/.kube/config
-chmod 755 /home/vagrant/.kube/config
-
-# cp /vagrant_data/JOIN_NETWORK.sh /home/vagrant/JOIN_NETWORK.sh
-# chmod +x /home/vagrant/JOIN_NETWORK.sh
-# /home/vagrant/JOIN_NETWORK.sh
+chown vagrant:vagrant /home/vagrant/.kube/config
 
 
 
+echo "[TASK ] Downloading and Installing the mount Components"
+apt update -qq >/dev/null 2>&1
+sudo apt install -qq -y nfs-common >/dev/null 2>&1
+
+echo "[TASK ] Creating Mount Points and Mounting Directories on the Client"
+mkdir -p /nfs/PV
+mount master:/var/nfs/PV /nfs/PV
